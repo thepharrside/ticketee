1ba695180c49820798abed11f090b0261fab9f52
thePharrSide <pharr.james@gmail.com>
2017-01-25 08:13:31 -0500

    Tickets can now be edited

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -33,6 +33,15 @@ end
 def show
 end
 
+def update
+	if @ticket.update(ticket_params)
+		flash[:notice] = "Ticket has been updated."
+		redirect_to [@project, @ticket]
+	else
+		flash.now[:alert] = "Ticket has not been updated."
+		render "edit"
+	end
+end
 
 private
 
