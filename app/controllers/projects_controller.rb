0722f1b2710e0bb22fe17a49ba81918f56ffe879
thePharrSide <pharr.james@gmail.com>
2017-01-27 15:08:24 -0500

    Handle authorization errors gracefully

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -42,6 +42,6 @@ class ProjectsController < ApplicationController
 		end
 
 
-	end
+end
 
 
--- spec/controllers/admin/projects_controller_spec.rb
+++ spec/controllers/admin/projects_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe Admin::ProjectsController, type: :controller do
+RSpec.describe ProjectsController, type: :controller do
 
+  it "handles permission errors by redirecting to a safe place" do
+  	allow(controller).to receive(:current_user)
+
+  	project = FactoryGirl.create(:project)
+  	get :show, params: { id: project }
+
+  	expect(response).to redirect_to(root_path)
+  	message = "You aren't allowed to do that."
+  	expect(flash[:alert]).to eq message
+  end
 end
