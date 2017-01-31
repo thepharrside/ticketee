5248f94680c77da63d4824e9e1d74fe29d0cd435
thePharrSide <pharr.james@gmail.com>
2017-01-31 11:01:11 -0500

    Verify that we use authorization in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :not_authorized
 
+  after_action :verify_authorized, except: [:index], unless: :devise_controller?
+  after_action :verify_policy_scoped, only: [:index], unless: :devise_controller?
+
   private
     def not_authorized
     	redirect_to root_path, alert: "You aren't allowed to do that."
