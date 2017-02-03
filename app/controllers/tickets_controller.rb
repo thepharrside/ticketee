0875be54b55e876417e8c5530d197da58c5f475f
thePharrSide <pharr.james@gmail.com>
2017-02-03 09:32:16 -0500

    File uploads are persisted across form re-displays

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -54,7 +54,7 @@ end
 private
 
 def ticket_params
-	params.require(:ticket).permit(:name, :description, :attachment)
+	params.require(:ticket).permit(:name, :description, :attachment, :attachment_cache)
 end
 
 def set_project
